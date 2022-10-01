67e760782f81e49d1ba4c6a45ed0149e14c490eb
Kuan Heng (Jordan) Lin <kuanhenglin@outlook.com>
2022-10-01 12:25:19 -0400

    jordan: set up client folder
    Basic markdown reader set up with basic CSS. Navigation bar is yet to be
    implemented and is the next step.
    
    A sample article has been included as well. Editing articles (currently)
    requires editing the string defined in src/articles/articles.js.

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import './App.css';
+
+import { Home } from "./pages/Home.js";
+import { Article } from "./pages/Article.js"
+
+
+function App() {
+  return (
+    <div className="body">
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={ <Home /> }/>
+          <Route path="/article" element={ <Article link="../articles/ucla.md" /> }/>
+        </Routes>
+      </HashRouter>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ client/src/articles/articles.js
@@ -0,0 +1,39 @@
+function articles(name) {
+  switch(name) {
+    case "ucla":
+      return `
+# University of California, Los Angeles
+
+![UCLA Royce Hall](https://s3.amazonaws.com/cms.ipressroom.com/173/files/20198/5d72b4772cfac209ff04c634_Royce+Quad/Royce+Quad_hero.jpg)
+<figcaption>UCLA Royce Hall, the oldest building of the university.</figcaption>
+
+The University of California, Los Angeles (UCLA) is a public land-grant research university in Los Angeles, California. UCLA's academic roots were established in 1881 as a teachers college then known as the southern branch of the California State Normal School (now San José State University). This school was absorbed with the official founding of UCLA as the Southern Branch of the University of California in 1919, making it the second-oldest of the 10-campus University of California system (after UC Berkeley).
+
+## Campus
+
+The new UCLA campus in 1929 had four buildings: Royce Hall and Haines Hall on the north, and Powell Library and Kinsey Hall (now called Renee And David Kaplan Hall) on the south. The Janss Steps were the original 87-step entrance to the university that lead to the quad of these four buildings. Today, the campus includes 163 buildings across 419 acres (1.7 km<sup>2</sup>) in the western part of Los Angeles, north of the Westwood shopping district and just south of Sunset Boulevard. In terms of acreage, it is the second-smallest of the ten UC campuses. The campus is approximately 1 mile east of I-405 (the San Diego Freeway).
+
+**Source**: [*Wikipedia*](https://en.wikipedia.org/wiki/University_of_California,_Los_Angeles)
+
+### Miscellaneous
+
+\`\`\`
+print("Hello, World!")
+\`\`\`
+
+1. Peanut
+2. Butter
+3. Jelly
+4. Jsandwich
+
+| Peanut  | Butter | Jelly   | Jsandwich |
+| ------- | ------ | ------- | --------- |
+| Prateik | Benson | Jeffrey | Jordan    |
+
+**Text** of *all*&mdash;varieties! Make sure to drink lots of H<sub>2</sub>O :) Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      `;
+    }
+}
+
+
+export { articles };
