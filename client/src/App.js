a3f37abcdeb40a634de6113aa61943554811e4c5
Benson Liu <bensonhliu@gmail.com>
2022-10-01 13:33:55 -0400

    fixed CI/CD

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 import { Home } from "./pages/Home.js";
-import { Article } from "./pages/Article.js"
-
+import { Article } from "./pages/Article.js";
 
 function App() {
   return (
     <div className="body">
       <HashRouter>
         <Routes>
-          <Route path="/" element={ <Home /> }/>
-          <Route path="/article" element={ <Article link="../articles/ucla.md" /> }/>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/article"
+            element={<Article link="../articles/ucla.md" />}
+          />
         </Routes>
       </HashRouter>
     </div>
   );
 }
-
 
 export default App;
