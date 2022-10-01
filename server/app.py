8fb1ec47560aed013bef9c18cd3ac8ef1fdeea40
hametar0u <57603310+hametar0u@users.noreply.github.com>
2022-10-01 12:40:51 -0400

    user creation + query

--- server/app.py
+++ server/app.py
@@ -1,8 +1,34 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wikisafe.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def test():
+  return "hello world"
 
-app.run()
+@app.route('/create_user/<usr>')
+def create_user(usr):
+  user = User(username=usr, email=f'{usr}@example.com')
+  db.session.add(user)
+  db.session.commit()
+  return f"user {usr} created!"
+
+@app.route('/get_user/<usr>')
+def get_user(usr):
+  print(User.query.filter_by(username=usr), file=sys.stderr)
+  return f"user {usr} found"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+if __name__ == '__main__':
+  app.run()
